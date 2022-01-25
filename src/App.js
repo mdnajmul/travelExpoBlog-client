ddbd1dc4ebadd2e98f31c3d0fc0b00d380a37a33
mdnajmul <mdnajmulovi@gmail.com>
2022-01-25 23:03:28 +0600

    create context api for state management

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import AuthProvider from "./context/AuthProvider";
 import Home from "./pages/Home/Home/Home";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/home" element={<Home />} />
-    </Routes>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import initializeAuthentication from "./pages/Login/Firebase/firebase.init";
-
-initializeAuthentication();
 
 ReactDOM.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
