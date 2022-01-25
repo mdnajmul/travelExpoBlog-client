6739ffe7b5bfe4c13f683b0a2a2348824dd8a2d3
mdnajmul <mdnajmulovi@gmail.com>
2022-01-26 03:00:02 +0600

    responsive navigation bar design complete

--- src/pages/Shared/Navigation/Navigation.js
+++ src/pages/Shared/Navigation/Navigation.js
@@ -1,10 +1,94 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import useAuth from "../../../hooks/useAuth";
+import "./Navigation.css";
 
 const Navigation = () => {
+  const { user, logOut } = useAuth();
   return (
-    <div>
-      <h2>This is navigation</h2>
-    </div>
+    <nav class="navbar navbar-expand-lg header-nav shadow-lg">
+      <div class="container-fluid">
+        <span
+          style={{
+            color: "white",
+            fontWeight: "bold",
+            fontSize: "28px",
+          }}
+        >
+          travel<span style={{ color: "red" }}>Expo</span>Blog
+        </span>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+            <Link to="/home" className="nav-link topic">
+              Home
+            </Link>
+            <li class="nav-item">
+              <Link to="/about-us" className="nav-link topic">
+                About Us
+              </Link>
+            </li>
+
+            <li class="nav-item">
+              <Link to="/allblogs" className="nav-link topic">
+                All Blogs
+              </Link>
+            </li>
+
+            {user?.email && (
+              <li className="nav-item  fitpal-nav-item me-4">
+                <Link className="nav-link topic" to="/dashboard">
+                  Dashboard
+                </Link>
+              </li>
+            )}
+          </ul>
+
+          {/* {user.email && (
+            <div className="mx-3">
+              <img
+                style={{ width: "40px", height: "40px", borderRadius: "50%" }}
+                src={user?.photoURL}
+                alt=""
+              />
+              <span className="text-light">
+                {" "}
+                {user?.displayName} | {admin ? "Admin" : "Subscriber"}
+              </span>
+            </div>
+          )} */}
+
+          <ul class="navbar-nav  mb-2 mb-lg-0">
+            {user?.email ? (
+              <button
+                onClick={logOut}
+                style={{ outline: 0, border: 0, borderRadius: "3px" }}
+              >
+                LogOut
+              </button>
+            ) : (
+              <li className="nav-item fitpal-nav-item me-4">
+                <Link className="nav-link topic login-btn" to="/login">
+                  Login
+                </Link>
+              </li>
+            )}
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
